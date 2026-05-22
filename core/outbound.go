c18851a76594548f2313b7e6aaaec64fdb619713
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2026-05-22 22:38:43 +0800

    Update outbound.go

--- core/outbound.go
+++ core/outbound.go
@@ -18,7 +18,7 @@ func buildDefaultOutbound() (*core.OutboundHandlerConfig, error) {
 	// outboundDetourConfig.SendThrough = &sendthrough
 
 	proxySetting := &conf.FreedomConfig{
-		DomainStrategy: "UseIPv4v6",
+		DomainStrategy: "ForceIPv4",
 	}
 	var setting json.RawMessage
 	setting, err := json.Marshal(proxySetting)
