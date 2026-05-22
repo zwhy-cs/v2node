c021636b72cef4cbbe91c2732f44a1513f7493af
zwhy <125182767+zwhy-cs@users.noreply.github.com>
2026-05-22 22:48:32 +0800

    Update custom.go

--- core/custom.go
+++ core/custom.go
@@ -44,7 +44,7 @@ func hasOutboundWithTag(list []*core.OutboundHandlerConfig, tag string) bool {
 
 func GetCustomConfig(infos []*panel.NodeInfo) (*dns.Config, []*core.OutboundHandlerConfig, *router.Config, error) {
 	//dns
-	queryStrategy := "UseIPv4v6"
+	queryStrategy := "UseIPv4"
 	// if !hasPublicIPv6() {
 	// 	queryStrategy = "UseIPv4"
 	// }
